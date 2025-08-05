fb4e0c58dca6d6991034e0d6eb1398ca5cd207fb
Zeno Maccari <zeno.maccari@studio.unibo.it>
2025-08-05 12:39:53 +0200

    Finished Week 4

--- Week4/HW4.hs
+++ Week4/HW4.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE TemplateHaskell #-}
+module Week4.HW4 where
+import Data.List ((\\))
 {-
 Exercise 1: Wholemeal programming
 Reimplement each of the following functions in a more idiomatic
@@ -10,11 +13,25 @@ fun1 [] = 1
 fun1 (x:xs)
  | even x = (x - 2) * fun1 xs
  | otherwise = fun1 xs
+
 fun2 :: Integer -> Integer
 fun2 1 = 0
-fun2 n | even n = n + fun2 (n `div` 2)
+fun2 n
+ | even n = n + fun2 (n `div` 2)
  | otherwise = fun2 (3 * n + 1)
 
+--First possibility
+fun1' :: [Integer] -> Integer
+fun1'  = product . map (subtract 2) . filter even
+--Second possibility
+fun1'' :: [Integer] -> Integer
+fun1'' xs = product [x-2 | x <- xs, even x]
+
+fun2' :: Integer -> Integer
+fun2' = sum . filter even . takeWhile (>1) .iterate acc where
+    acc n
+     | even n = div n 2
+     | otherwise = 3*n +1
 
 {-Exercise 2: Folding with trees
 Recall the definition of a binary tree data structure. The height of http://en.wikipedia.org/wiki/
@@ -36,7 +53,20 @@ data Tree a = Leaf
  | Node Integer (Tree a) a (Tree a)
  deriving (Show, Eq)
 
+foldTree :: [a] -> Tree a
+foldTree = foldr insert Leaf
 
+insert :: a -> Tree a -> Tree a
+insert a Leaf = Node 0 Leaf a Leaf
+insert a (Node h t_l b t_r)
+    | height t_l < height t_r = let newTree = insert a t_l in
+         Node (1 + max (height newTree) (height t_r)) newTree b t_r
+    | otherwise =let newTree = insert a t_r in
+         Node (1 + max (height t_l) (height newTree)) t_l b newTree
+
+height :: Tree a -> Integer
+height Leaf = -1
+height (Node h _ _ _) = h
 
 
 {-Exercise 3: More folds!
@@ -44,8 +74,10 @@ data Tree a = Leaf
 xor :: [Bool] -> Bool
 which returns True if and only if there are an odd number of True
 values contained in the input list. It does not matter how many
-False values the input list contains.-} 
+False values the input list contains.-}
 
+xor :: [Bool] -> Bool
+xor = foldr (\ x y -> (x && not y) || (y && not x) ) False
 
 
 {-Implement map as a fold. That is, complete the definition
@@ -54,7 +86,8 @@ map’ f = foldr ...
 in such a way that map’ behaves identically to the standard map
 function.-}
 
-
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x y -> f x : y)  []
 
 {-(Optional) Implement foldl using foldr. That is, complete the
 definition
@@ -63,7 +96,8 @@ myFoldl f base xs = foldr ...
 in such a way that myFoldl behaves identically to the standard
 foldl function.-}
 
-
+myFoldl :: (a -> b -> a) -> a -> [b] -> a
+myFoldl f  = foldr (flip f)
 
 {-Exercise 4: Finding primes
 Read about the Sieve of Sundaram. Implement the algorithm us- http://en.wikipedia.org/wiki/Sieve_
@@ -71,3 +105,17 @@ of_Sundaram
 ing function composition. Given an integer n, your function should
 generate all the odd prime numbers up to 2n + 2.
 sieveSundaram :: Integer -> [Integer] -}
+--sieveSundaram :: Integer -> [Integer]
+--sieveSundaram n = [x | x <-[1..j], i<-[1..j] , not (x == i+j+2*i*j)] 
+{-
+[((x,i),j) | (x,i) <- [(x,i)| x<-[1..n], i<-[1..j]], j <[1..j]
+-}
+
+createIJlist :: (Num b, Enum b, Ord b) => b -> [(b, b)]
+createIJlist n  = [(i,j) | i<-[1..n], j<-[1..n], i <=j, i+j + (2*i*j) <= n ]
+
+sumIJlist :: (Num b, Enum b, Ord b) => [(b,b)] -> [b]
+sumIJlist = map (\(i,j)-> i + j + (2*i*j))
+
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = map ((+1).(*2)) $ [1..n] \\ sumIJlist (createIJlist n)
